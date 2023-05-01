5fbc4096ba15607a4456fa09f5c1453e8bcefd8d
DESKTOP-S27RSRD\Str <hamzambf@gmail.com>
2023-05-01 21:59:34 +0500

    Till styling

--- React/react-basic/src/components/ComponentA.js
+++ React/react-basic/src/components/ComponentA.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ComponentB from "./ComponentB";
+import "./style.css";
 
 const ComponentA = ({ name, address, click }) => {
   return (
     <>
-      <h1>My name is {name}</h1>
-      <h1>My address is {address}</h1>
+      <h1 className="heading">My name is {name}</h1>
+      <h1 className="text-red-500 bg-black text-[32px] capitalize px-[10px] py-[10px]">
+        My address is {address}
+      </h1>
       <button onClick={click}>Click</button>
       <ComponentB newName={name} />
     </>
@@ -13,3 +16,16 @@ const ComponentA = ({ name, address, click }) => {
 };
 
 export default ComponentA;
+
+// class ComponentA extends React.Component {
+//   name = "Hamza";
+//   render() {
+//     return (
+//       <>
+//         <h1>My name is {this.name}</h1>
+//       </>
+//     );
+//   }
+// }
+
+// export default ComponentA;
